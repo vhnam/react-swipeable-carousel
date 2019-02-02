76d8bf5d29059e355d62a3d002f341090c4b7818
Nam Vo <vhnam2504@gmail.com>
2019-02-02 17:07:12 +0700

    fix(setup): export components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import Carousel from './components/Carousel';
 import GridCarousel from './components/GridCarousel';
 
-module.exports = {
-  Carousel,
-  GridCarousel,
-};
+export {Carousel, GridCarousel};
