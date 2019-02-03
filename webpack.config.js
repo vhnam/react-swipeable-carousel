298321305099c810ced84a8acc0c408c0af1fdf6
Nam Vo <vhnam2504@gmail.com>
2019-02-03 17:57:46 +0700

    fix(setup): fix compiler

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-    libraryTarget: 'commonjs2',
+    libraryTarget: 'commonjs2'
   },
   module: {
     rules: [
@@ -31,8 +31,5 @@ module.exports = {
         exclude: /node_modules/,
       },
     ],
-  },
-  externals: {
-    react: 'commonjs react',
-  },
+  }
 };
